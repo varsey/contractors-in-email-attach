63ad3f349f43fe4beddd045f9a6aeb662f733ef6
Evgenii Varseev <eugeny.varseev@gmail.com>
2024-08-21 08:21:34 +0300

    feat - read health_check.py envs from dotenv file

--- health_check.py
+++ health_check.py
@@ -4,9 +4,13 @@ import smtplib
 import requests
 from email.mime.text import MIMEText
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 SERVER_URL = 'http://0.0.0.0:8000'
-CHECK_INTERVAL = 1800
+CHECK_INTERVAL = 10
 
 SMTP_SERVER = 'smtp.yandex.ru'
 SMTP_PORT = 587
@@ -26,13 +30,13 @@ def send_email_alert():
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = SMTP_USERNAME
-    msg['To'] = TO_EMAIL
+    msg['To'] = ", ".join(TO_EMAIL.split(','))
 
     try:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls()
             server.login(SMTP_USERNAME, SMTP_PASSWORD)
-            server.sendmail(SMTP_USERNAME, TO_EMAIL, msg.as_string())
+            server.sendmail(SMTP_USERNAME, TO_EMAIL.split(','), msg.as_string())
         print(f'Alert email sent to {TO_EMAIL}')
     except Exception as e:
         print(f'Failed to send email: {e}')
