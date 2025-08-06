946d13fefd9d924cac410a3ec78e34b686cdfc48
Evgenii Varseev <e.varseev@nextbi.ru>
2025-08-06 13:10:08 +0300

    feat - return 500 errors

--- app.py
+++ app.py
@@ -20,10 +20,17 @@ def route_status():
 
 @app.route("/api/parsing/")
 def contractor_parser_parameter():
-    email_id = request.args.get('mid')
-    card = prc.process_email_by_id(message_id=email_id)
-    gc.collect()
-    return jsonify(card)
+    try:
+        email_id = request.args.get('mid')
+        card = prc.process_email_by_id(message_id=email_id)
+        gc.collect()
+
+        return jsonify(card)
+
+    except Exception as e:
+        app.logger.error(f"Error processing request: {str(e)}")
+
+        return jsonify({'error': 'Internal Server Error'}), 500
 
 
 if __name__ == "__main__":
